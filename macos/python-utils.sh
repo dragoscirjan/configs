62c5d9cbdb74bc261f630403c2304c64ba40056c
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2019-06-13 11:24:58 +0300

    macos iterm + python utils (bash_profile)

---
+++ macos/python-utils.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+
+sudo pip install virtualenvwrapper
+
+cat ~/.bash_profile | grep "virtualenvwrapper.sh" || {
+	mkdir -p $HOME/.virtualenvs
+	cat >> ~/.bash_profile <<EXP
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Devel
+source /usr/local/bin/virtualenvwrapper.sh
+EXP
+}
