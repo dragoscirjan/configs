162bbb044c73aec2edfa7006cca7e523b7f8b470
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2019-06-19 00:03:20 +0300

    Update install-jq.sh

--- git-bash/install-jq.sh
+++ git-bash/install-jq.sh
@@ -1,1 +1,7 @@
 #! /bin/bash
+
+binary=$(curl -sSL https://github.com/stedolan/jq/releases | grep jq-win64.exe | head -n 1 | awk -F'"' '{print $2}')
+
+mkdir -p ~/bin
+
+curl -sSL https://github.com$binary > jq.exe
