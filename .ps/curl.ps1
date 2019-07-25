800ff579f3478e0aa99cc598ac7f90654c5a2622
Dragos Cirjan <dragos.cirjan@gmail.com>
2019-07-25 20:05:59 +0300

    +

--- .ps/curl.ps1
+++ .ps/curl.ps1
@@ -8,11 +8,14 @@ param(
 # @see https://stackoverflow.com/questions/41618766/powershell-invoke-webrequest-fails-with-ssl-tls-secure-channel
 [Net.ServicePointManager]::SecurityProtocol = 
   [Net.SecurityProtocolType]::Tls12 -bor `
-  [Net.SecurityProtocolType]::Tls11 -bor `
-  [Net.SecurityProtoolType]::Tls
+  [Net.SecurityProtocolType]::Tls11 
+  #-bor `
+  #[Net.SecurityProtoolType]::Tls
 
 if ($output) {
-    Invoke-WebRequest -Uri "$url" -OutFile "$output"
+    $ProgressPreference = 'SilentlyContinue'
+    Measure-Command { Invoke-WebRequest -Uri "$url" -OutFile "$output" }
+    $ProgressPreference = 'Continue'
 } else {
     (new-object system.net.webclient).DownloadString($url)
 }
--- ide/intellij.ps1
+++ ide/intellij.ps1
@@ -1,7 +1,14 @@
 param(
-    [Parameter(Mandatory=$true)][String]$url=''
+    [Parameter(Mandatory=$true)][String]$url='',
+    [Parameter(Mandatory=$true)][String]$keys=''
 )
 
 $output = (new-object system.net.webclient).DownloadString($url) | ConvertFrom-Json
 
-echo $output.IIU.downloads.windows.link
+foreach ($key in $keys.split(',')) {
+    $link = $output.$key.downloads.windows.link
+    $file = Split-Path $link -leaf
+    # echo $link 
+    # echo $file
+    make --directory=../.install iu URL="$link" FILE="~/tmp/$file"
+}
