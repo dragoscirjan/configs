985645bff7be3623f0d8b3d6219bdb0a08e32ea6
Dragos Cirjan <dragos.cirjan@gmail.com>
2019-08-02 00:11:19 +0300

    +

--- .ps/install-font.ps1
+++ .ps/install-font.ps1
@@ -1,37 +1,10 @@
-param(
-	[String]$url='',
-	[Parameter(Mandatory=$false)][ValidateSet("true", "false")][String]$isFile="false"
-)
-
-# @see https://stackoverflow.com/questions/41618766/powershell-invoke-webrequest-fails-with-ssl-tls-secure-channel
-[Net.ServicePointManager]::SecurityProtocol = 
-  [Net.SecurityProtocolType]::Tls12 -bor `
-  [Net.SecurityProtocolType]::Tls11 -bor `
-  [Net.SecurityProtocolType]::Tls
-
-# [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-
-switch($isFile.ToLower()) {
-    "true" {
-        mkdir .\fonts
-        # -SkipCertificateCheck
-        Invoke-WebRequest -Uri "$url" -OutFile .\fonts\font.ttf   
-    }
-    default {
-        # -SkipCertificateCheck
-        Invoke-WebRequest -Uri "$url" -OutFile fonts.zip
-        Expand-Archive .\fonts.zip -DestinationPath .\fonts
-        Remove-Item fonts.zip
-    }
-}
-
-
 $ShellAppFontNamespace = 0x14
 $ShellApp = New-Object -ComObject Shell.Application
 $FontsFolder = $ShellApp.NameSpace($ShellAppFontNamespace)
 
-Get-Childitem -Path .\fonts -Include *ttf -File -Recurse -ErrorAction SilentlyContinue| Foreach {	Write-Verbose -Message ('Installing font: {0}' -f $_.fullname)
+Get-Childitem -Path ~\tmp\fonts -Include *ttf -File -Recurse -ErrorAction SilentlyContinue | Foreach {
+    Write-Verbose -Message ('Installing font: {0}' -f $_.fullname)
     $FontsFolder.CopyHere($_.fullname)
 }
 
-Remove-Item -Recurse -Force fonts
+# Remove-Item -Recurse -Force fonts
