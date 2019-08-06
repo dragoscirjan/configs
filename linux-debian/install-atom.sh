bd5c5afa5a2dd3e017e9b6c7c3f879930545ca97
Dragos Cirjan <dragos.cirjan@gmail.com>
2019-08-07 00:04:35 +0300

    started ide on linux

--- linux-debian/install-atom.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-set -e
-
-which apt-get > /dev/null && {
-  curl -sL https://atom.io/download/deb > /tmp/atom.deb
-  dpkg -i /tmp/atom.deb || apt-get install -yf
-  rm /tmp/atom.deb
-}
--- linux-debian/install-gitkracken.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-set -e
-
-which apt-get > /dev/null && {
-  curl -sL https://release.gitkraken.com/linux/gitkraken-amd64.deb > /tmp/kraken.deb
-  dpkg -i /tmp/kraken.deb || apt-get install -yf
-  rm /tmp/kraken.deb
-}
