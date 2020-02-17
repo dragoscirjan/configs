89bd6ca109deb1dc1c5c9858ffd1bbc0475e16a8
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2020-02-17 23:46:39 +0200

    +

---
+++ docker/scripts/configure-containerd.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+set -xe
+
+[ -f /etc/modules-load.d/containerd.conf ] || cat > /etc/modules-load.d/containerd.conf <<EOF
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+# Setup required sysctl params, these persist across reboots.
+[ -f /etc/sysctl.d/99-kubernetes-cri.conf ] || cat > /etc/sysctl.d/99-kubernetes-cri.conf <<EOF
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sysctl --system
