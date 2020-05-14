d3b3bf29b7a41ea9d4b7155adba8954d874d7728
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2020-05-14 03:54:46 +0300

    font fixes

--- fonts/install-font.ps1
+++ fonts/install-font.ps1
@@ -6,9 +6,23 @@ $ShellAppFontNamespace = 0x14
 $ShellApp = New-Object -ComObject Shell.Application
 $FontsFolder = $ShellApp.NameSpace($ShellAppFontNamespace)
 
-Get-Childitem -Path ~\tmp\fonts -Include *$ext -File -Recurse -ErrorAction SilentlyContinue | Foreach {
-    Write-Verbose -Message ('Installing font: {0}' -f $_.fullname)
-    $FontsFolder.CopyHere($_.fullname)
+Start-Job -Name "DetectAndClosePrompt" -Scriptblock {
+  $i=1
+  [void] [System.Reflection.Assembly]::LoadWithPartialName("'System.Windows.Forms")
+  [void] [System.Reflection.Assembly]::LoadWithPartialName("'Microsoft.VisualBasic")
+  while ($i -eq 1) {
+    $windowPrompt = Get-Process -ErrorAction SilentlyContinue |? {$_.MainWindowTitle -like "*Installing Fonts*"}
+    [Microsoft.VisualBasic.Interaction]::AppActivate($windowPrompt.ID)
+    [System.Windows.Forms.SendKeys]::SendWait("{Enter}")
+    sleep 2
+  }
 }
+
+Get-Childitem -Path ~\Downloads\fonts -Include *$ext -File -Recurse -ErrorAction SilentlyContinue | Foreach {
+    Write-Host "Installing font: $_"
+    $FontsFolder.CopyHere($_.fullname, 16+4)
+}
+
+Get-Job DetectAndClosePrompt | Remove-Job -Force
 
 # Remove-Item -Recurse -Force fonts
