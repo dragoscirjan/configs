d035bc650786eb723a373b7205961cbc586afdcc
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2021-03-22 01:27:49 +0200

    +

--- linux/rpi3-1/pi-hole/init.sh
+++ linux/rpi3-1/pi-hole/init.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-systemctl status systemd-resolved | grep "Active: active" \
-  && ( \
-    systemctl disable systemd-resolved.service \
-    && systemctl stop systemd-resolved \
-  )
+# systemctl status systemd-resolved | grep "Active: active" \
+#   && ( \
+#     systemctl disable systemd-resolved.service \
+#     && systemctl stop systemd-resolved \
+#   )
 
 docker-compose -f docker-compose.yml up -d
