74e44f14d9da00a1d9bfa7351579f2b1e0410bdd
Dragos Cirjan <dragos.cirjan@gmail.com>
2025-06-22 02:22:28 +0300

    chore: linux fix

--- shell/ohmyposh.bash.sh
+++ shell/ohmyposh.bash.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_PATH")" && pwd)"
 
 # Only initialize Oh My Posh if not running in Apple Terminal
 if [ "$TERM_PROGRAM" != "Apple_Terminal" ]; then
-  eval "$(oh-my-posh init bash --config $SCRIPT_DIR/ohmyposh.config.toml)"
+  eval "$($HOME/.local/bin/oh-my-posh init bash --config $SCRIPT_DIR/ohmyposh.config.toml)"
 fi
